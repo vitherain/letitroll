f1bcd83753427dae0ca2f84f5180283f125c3114
Vít Herain <vit.herain@zonky.cz>
2018-03-10 00:29:03 +0100

    Initial commit

---
+++ be/src/main/java/io/letitroll/be/LetItRollApplication.java
@@ -0,0 +1,12 @@
+package io.letitroll.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetItRollApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(BeApplication.class, args);
+	}
+}

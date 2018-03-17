5429dcb21969e38a55a57f7e997c991e3089e469
Vít Herain <vit.herain@zonky.cz>
2018-03-17 12:12:23 +0100

    Added sample emitter almost working now

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -1,16 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as Rx from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  eventSource: any;
+
+  EventSource: any = window['EventSource'];
+
+  private onMessageSubj: Rx.Subject<String> = new Rx.Subject();
+
+  onMessage$: Rx.Observable<String> = this.onMessageSubj.asObservable();
+
+  ngOnInit(): void {
+    //this.onMessage$.subscribe(console.log);
+  }
 
   createEmitter(): void {
-    debugger
-    const eventSource: EventSource = new EventSource('http://localhost:8080/emitter');
-    console.log(eventSource)
+    this.eventSource = new this.EventSource('http://localhost:8080/emitter');
+    this.eventSource.onmessage = (msg) => {
+      const data: String = JSON.parse(msg.data).message;
+      console.log('came message', data);
+      this.onMessageSubj.next(data);
+    };
   }
 }
--- be/src/main/java/io/letitroll/be/endpoint/EmitterController.java
+++ be/src/main/java/io/letitroll/be/endpoint/EmitterController.java
@@ -1,7 +1,9 @@
 package io.letitroll.be.endpoint;
 
 import io.letitroll.be.service.EventService;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -9,16 +11,21 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 public class EmitterController {
 
-    private List<SseEmitter> emitters = new ArrayList<>();
+    private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
 
+    @CrossOrigin
     @GetMapping("/emitter")
     public SseEmitter getSseEmitter() throws IOException {
         final SseEmitter emitter = new SseEmitter();
-        emitter.send(SseEmitter.event().data("Message #1"));
+
+        emitter.onCompletion(() -> this.emitters.remove(emitter));
+        emitter.onTimeout(() -> this.emitters.remove(emitter));
+
         emitters.add(emitter);
         return emitter;
     }
@@ -28,6 +35,7 @@ public class EmitterController {
         List<SseEmitter> deadEmitters = new ArrayList<>();
         this.emitters.forEach(emitter -> {
             try {
+                LoggerFactory.getLogger(this.getClass()).info("sending message {}", event.getMessage());
                 emitter.send(event);
             }
             catch (final Exception e) {

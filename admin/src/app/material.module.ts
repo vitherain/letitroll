394479ad8822fb168d79795986257e8dade3aae8
Vít Herain <vit.herain@zonky.cz>
2018-03-18 23:16:40 +0100

    Not working menu

--- admin/src/app/material.module.ts
+++ admin/src/app/material.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatMenuModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatCheckboxModule]
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatMenuModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatMenuModule
+  ]
 })
 export class MaterialModule { }

6ce9bc57f1c7095586d4a044d19416f72ba435b3
Vít Herain <vit.herain@zonky.cz>
2018-03-25 15:46:17 +0200

    Configured embedded Mongo for tests

---
+++ be/src/main/java/io/letitroll/be/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package io.letitroll.be.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@Configuration
+@EnableReactiveMongoRepositories
+public class MongoConfig {
+}
---
+++ be/src/test/java/io/letitroll/be/LetItRollApplicationTest.java
@@ -0,0 +1,10 @@
+package io.letitroll.be;
+
+import org.junit.Test;
+
+public class LetItRollApplicationTest extends BaseSpringTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}

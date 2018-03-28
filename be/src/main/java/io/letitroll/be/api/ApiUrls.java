3ac434d659a1161b40f5f205789c8738342bd890
Vít Herain <vit.herain@zonky.cz>
2018-03-28 22:12:55 +0200

    Simple security config now works

--- be/src/main/java/io/letitroll/be/api/ApiUrls.java
+++ be/src/main/java/io/letitroll/be/api/ApiUrls.java
@@ -2,7 +2,7 @@ package io.letitroll.be.api;
 
 public final class ApiUrls {
 
-    public static final String API_GET_ALL_FEATURES = "/api/features";
+    public static final String API_GET_ALL_FEATURES = "/api/v1/features";
 
     private ApiUrls() {
     }
--- be/src/main/java/io/letitroll/be/security/config/SecurityConfig.java
+++ be/src/main/java/io/letitroll/be/security/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package io.letitroll.be.security.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -12,11 +15,22 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
 
+    @Value("${letitroll.bcrypt-rounds}")
+    private Integer bCryptRounds;
+
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(final ServerHttpSecurity http) {
-        return http.authorizeExchange()
-                .anyExchange().permitAll()/*.authenticated()*/
-                .and().formLogin()
-                .and().build();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(bCryptRounds);
+    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(final ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .httpBasic().and()
+                .formLogin();
+        return http.build();
     }
 }

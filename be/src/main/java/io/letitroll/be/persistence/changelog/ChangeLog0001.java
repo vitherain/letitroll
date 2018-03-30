559785a9738371ce0be645397f298dc31b8b466f
Vít Herain <vit.herain@zonky.cz>
2018-03-30 08:50:20 +0200

    Sample commented mongobee changelog class

---
+++ be/src/main/java/io/letitroll/be/persistence/changelog/ChangeLog0001.java
@@ -0,0 +1,17 @@
+package io.letitroll.be.persistence.changelog;
+
+import com.github.mongobee.changeset.ChangeLog;
+import com.github.mongobee.changeset.ChangeSet;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+
+//@ChangeLog
+public class ChangeLog0001 {
+
+    /*@ChangeSet(order = "0001", id = "tmp", author = "vherain")
+    public void importantWorkToDo(final DB db){
+        System.out.println(db);
+        final DBCollection users = db.getCollection("users");
+        System.out.println(users);
+    }*/
+}

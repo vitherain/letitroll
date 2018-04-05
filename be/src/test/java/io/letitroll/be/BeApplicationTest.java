018a40ecc38ca3924190a918a4f58123757dc08a
Vít Herain <vit.herain@zonky.cz>
2018-04-05 23:14:41 +0200

    Try to fix be build

--- be/src/test/java/io/letitroll/be/BeApplicationTest.java
+++ be/src/test/java/io/letitroll/be/BeApplicationTest.java
@@ -1,8 +1,24 @@
 package io.letitroll.be;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 public class BeApplicationTest extends BaseSpringTest {
+
+    /**
+     * Temporary
+     */
+    @TestConfiguration
+    static class Config {
+
+        @Bean
+        public RabbitTemplate rabbitTemplate() {
+            return Mockito.mock(RabbitTemplate.class);
+        }
+    }
 
     @Test
     public void contextLoads() {

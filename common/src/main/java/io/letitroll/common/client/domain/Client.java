1ac36eaaaf4c7e98b7ec0067a0eeb367dd9f4441
Vít Herain <vit.herain@zonky.cz>
2018-04-11 21:43:07 +0200

    Added Project and Environment into Client entity

--- common/src/main/java/io/letitroll/common/client/domain/Client.java
+++ common/src/main/java/io/letitroll/common/client/domain/Client.java
@@ -1,9 +1,12 @@
 package io.letitroll.common.client.domain;
 
+import io.letitroll.common.environment.Environment;
+import io.letitroll.common.project.domain.Project;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
@@ -25,6 +28,12 @@ public final class Client {
     private final ObjectId id;
     @Version
     private final long version;
+    @DBRef
+    @NotNull
+    private final Project project;
+    @DBRef
+    @NotNull
+    private final Environment environment;
     @NotNull
     @Size(max = 255)
     private final String key;
@@ -34,14 +43,20 @@ public final class Client {
     private final String email;
     private final boolean anonymous;
 
-    public Client(@NonNull final String key, final boolean anonymous) {
-        this(null, 0, key, null, null, anonymous);
+    public Client(
+            @NonNull final Project project,
+            @NonNull final Environment environment,
+            @NonNull final String key,
+            final boolean anonymous) {
+        this(null, 0, project, environment, key, null, null, anonymous);
     }
 
     @PersistenceConstructor
     public Client(
             @Nullable final ObjectId id,
             final long version,
+            @NonNull final Project project,
+            @NonNull final Environment environment,
             @NonNull final String key,
             @Nullable final String ip,
             @Nullable final String email,
@@ -49,6 +64,8 @@ public final class Client {
 
         this.id = id;
         this.version = version;
+        this.project = project;
+        this.environment = environment;
         this.key = key;
         this.ip = ip;
         this.email = email;
@@ -62,6 +79,16 @@ public final class Client {
 
     public long getVersion() {
         return version;
+    }
+
+    @NonNull
+    public Project getProject() {
+        return project;
+    }
+
+    @NonNull
+    public Environment getEnvironment() {
+        return environment;
     }
 
     @NonNull

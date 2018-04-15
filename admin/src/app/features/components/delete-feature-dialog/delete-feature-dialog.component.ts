6ca7736ee1e502554423895efc999dc72b2105ec
Vít Herain <vit.herain@zonky.cz>
2018-04-15 17:20:51 +0200

    data property is public

--- admin/src/app/features/components/delete-feature-dialog/delete-feature-dialog.component.ts
+++ admin/src/app/features/components/delete-feature-dialog/delete-feature-dialog.component.ts
@@ -14,7 +14,7 @@ export class DeleteFeatureDialogComponent {
 
   constructor(
     private dialogRef: MatDialogRef<DeleteFeatureDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) private data: any
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
   onCancel(): void {

f16555cf6fa9d7adaab771342943db6ea3cb5dcc
Vít Herain <vit.herain@zonky.cz>
2018-04-15 08:27:35 +0200

    BE integration tests repaired

--- be/src/test/java/io/letitroll/be/BaseSpringTest.java
+++ be/src/test/java/io/letitroll/be/BaseSpringTest.java
@@ -4,10 +4,12 @@ import io.letitroll.test.category.SlowIntegrationTest;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 @Category(SlowIntegrationTest.class)
 public abstract class BaseSpringTest {
 }

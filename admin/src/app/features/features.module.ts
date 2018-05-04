8f1393b467fa7f70b54c97768c86e42a97a2ec2a
Vít Herain <vit.herain@zonky.cz>
2018-05-04 22:53:49 +0200

    Reverted back changes related to ngrx-actions

--- admin/src/app/features/features.module.ts
+++ admin/src/app/features/features.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
 import { FeaturesEffects } from './store/features.effects';
-import { FeaturesStore, SideNavStore } from './store/features.store';
+import { featuresReducer, sideNavReducer } from './store/features.reducers';
 import { FeaturesComponent } from './components/features/features.component';
 import { FeaturesRoutingModule } from './features-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { FeaturesListComponent } from './components/features-list/features-list.component';
 import { DeleteFeatureDialogComponent } from './components/delete-feature-dialog/delete-feature-dialog.component';
 import { FormsModule } from '@angular/forms';
-import { NgrxActionsModule } from 'ngrx-actions';
 
 @NgModule({
   declarations: [FeaturesComponent, FeaturesListComponent, DeleteFeatureDialogComponent],
@@ -19,8 +19,8 @@ import { NgrxActionsModule } from 'ngrx-actions';
     CommonModule,
     FeaturesRoutingModule,
     SharedModule,
-    NgrxActionsModule.forFeature('features', FeaturesStore),
-    NgrxActionsModule.forFeature('sideNavOpened', SideNavStore),
+    StoreModule.forFeature('features', featuresReducer),
+    StoreModule.forFeature('sideNavOpened', sideNavReducer),
     EffectsModule.forFeature([FeaturesEffects]),
     FormsModule
   ]

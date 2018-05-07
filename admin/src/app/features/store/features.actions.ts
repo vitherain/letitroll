23cb30e9d37de2741c458e49c873c94832ee69aa
Vít Herain <vit.herain@zonky.cz>
2018-05-07 18:11:44 +0200

    Removed obsolete custom-action interface

--- admin/src/app/features/store/features.actions.ts
+++ admin/src/app/features/store/features.actions.ts
@@ -1,33 +1,33 @@
 import { TableRequestPayload } from '../../shared/tables/table-request.payload';
 import { HttpErrorInfo } from '../../shared/http/http-error-info';
-import { CustomAction } from '../../shared/store/custom-action';
 import { ListPayload } from '../../shared/store/list-payload';
 import { Feature } from '../models/feature.model';
+import { Action } from '@ngrx/store';
 
 export const LOAD_FEATURES = '[FEATURES] LOAD_FEATURES';
 export const LOAD_FEATURES_SUCCESS = '[FEATURES] LOAD_FEATURES_SUCCESS';
 export const LOAD_FEATURES_FAILURE = '[FEATURES] LOAD_FEATURES_FAILURE';
 export const TOGGLE_SIDE_NAV = '[FEATURES] TOGGLE_SIDE_NAV';
 
-export class LoadFeatures implements CustomAction {
+export class LoadFeatures implements Action {
   readonly type = LOAD_FEATURES;
 
   constructor(public payload: TableRequestPayload) {}
 }
 
-export class LoadFeaturesSuccess implements CustomAction {
+export class LoadFeaturesSuccess implements Action {
   readonly type = LOAD_FEATURES_SUCCESS;
 
   constructor(public payload: ListPayload<Feature>) {}
 }
 
-export class LoadFeaturesFailure implements CustomAction {
+export class LoadFeaturesFailure implements Action {
   readonly type = LOAD_FEATURES_FAILURE;
 
   constructor(public payload: HttpErrorInfo) {}
 }
 
-export class ToggleFeaturesSideNav implements CustomAction {
+export class ToggleFeaturesSideNav implements Action {
   readonly type = TOGGLE_SIDE_NAV;
 
   constructor() {}
--- admin/src/app/projects/store/projects.actions.ts
+++ admin/src/app/projects/store/projects.actions.ts
@@ -1,25 +1,25 @@
 import { HttpErrorInfo } from '../../shared/http/http-error-info';
-import { CustomAction } from '../../shared/store/custom-action';
 import { ListPayload } from '../../shared/store/list-payload';
 import { Project } from '../models/project.model';
+import { Action } from '@ngrx/store';
 
 export const LOAD_PROJECTS = '[PROJECTS] LOAD_PROJECTS';
 export const LOAD_PROJECTS_SUCCESS = '[PROJECTS] LOAD_PROJECTS_SUCCESS';
 export const LOAD_PROJECTS_FAILURE = '[PROJECTS] LOAD_PROJECTS_FAILURE';
 
-export class LoadProjects implements CustomAction {
+export class LoadProjects implements Action {
   readonly type = LOAD_PROJECTS;
 
   constructor() {}
 }
 
-export class LoadProjectsSuccess implements CustomAction {
+export class LoadProjectsSuccess implements Action {
   readonly type = LOAD_PROJECTS_SUCCESS;
 
   constructor(public payload: ListPayload<Project>) {}
 }
 
-export class LoadProjectsFailure implements CustomAction {
+export class LoadProjectsFailure implements Action {
   readonly type = LOAD_PROJECTS_FAILURE;
 
   constructor(public payload: HttpErrorInfo) {}
